13cdfa6cd66be80a7135e975f96a717ff939779e
Vincent Mallet <vmallet@gmail.com>
2021-04-16 22:11:08 -0700

    Initial Commit

---
+++ LeBackup/LeBackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeBackupApp.swift
+//  LeBackup
+//
+//  Created by Vincent Mallet on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LeBackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
